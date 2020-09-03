b595fa3e72fd1d7c64b628bdabae1c0cf7e52a99
@wwwjim <send@jimmy.email>
2020-09-03 00:14:19 -0700

    endpoint: all API routes in an endpoint for crawlers

---
+++ pages/api/index.js
@@ -0,0 +1,18 @@
+import * as Constants from "~/common/constants";
+
+import Cors from "cors";
+import initMiddleware from "~/common/init-middleware";
+
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(req, res) {
+  res
+    .status(200)
+    .send(
+      JSON.stringify({ decorator: "API", data: Constants.ROUTES }, null, 4)
+    );
+}
