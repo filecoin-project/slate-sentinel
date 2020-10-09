8383fd02f22c91f1232ef26a70f079553a33d6f1
@wwwjim <send@jimmy.email>
2020-10-08 21:27:06 -0700

    miners update

--- common/constants.js
+++ common/constants.js
@@ -1,16 +1,17 @@
 export const IGNORED_DEAL_BOTS = [
-  { id: "t1nslxql4pck5pq7hddlzym3orxlx35wkepzjkm3i", alias_id: "t0112" },
-  { id: "t1stghxhdp2w53dym2nz2jtbpk6ccd4l2lxgmezlq", alias_id: "t0113" },
-  { id: "t1mcr5xkgv4jdl3rnz77outn6xbmygb55vdejgbfi", alias_id: "t0114" },
-  { id: "t1qiqdbbmrdalbntnuapriirduvxu5ltsc5mhy7si", alias_id: "t010089" },
+  { id: "t1nslxql4pck5pq7hddlzym3orxlx35wkepzjkm3i", alias_id: "f0112" },
+  { id: "t1stghxhdp2w53dym2nz2jtbpk6ccd4l2lxgmezlq", alias_id: "f0113" },
+  { id: "t1mcr5xkgv4jdl3rnz77outn6xbmygb55vdejgbfi", alias_id: "f0114" },
+  { id: "t1qiqdbbmrdalbntnuapriirduvxu5ltsc5mhy7si", alias_id: "f010089" },
 ];
 
+// TODO(jim): Get this from the static miners list.
 export const PRIMARY_MINERS = [
-  "t016303",
-  "t016304",
-  "t016305",
-  "t016305",
-  "t016309",
+  "f016303",
+  "f016304",
+  "f016305",
+  "f016305",
+  "f016309",
 ];
 
 export const STATIC = ["/api/static-global-miners"];
